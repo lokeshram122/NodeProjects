dad6de879b7d48fe0f23e65da5f958de5de121a2
unknown <lokeshram122@gmail.com>
2022-02-15 16:05:40 +0530

    Next commit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+var express = require('express')
+
+
+var app = express();
+
+var router = express.Router();
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+app.post("/",(req,res)=>{
+    res.send("Hello " +req.body?.UserName)
+})
+
+
+
+var server = app.listen(5000,()=>
+{
+    console.log("listening to server " + server.address().port)
+})
